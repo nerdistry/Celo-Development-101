6b8a35187897eb91bb4d3c65837bd60e3b533684
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-04-05 14:07:20 +0300

    Reading Balance

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import marketplaceAbi from "../contract/marketplace.abi.json"
 import erc20Abi from "../contract/erc20.abi.json"
 
 const ERC20_DECIMALS = 18
-const MPContractAddress = "0x10F80800be45c963f3885Ff9625F5DaD51bbe8EB"
+const MPContractAddress = "0xF9E4F68754AE395a2658c9ae101bEd0070301268"
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 let kit
